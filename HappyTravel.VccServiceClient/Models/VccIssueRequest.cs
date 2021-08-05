b72183f34a76d6c310d9ab22ad0465670a9c0dcf
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-08-05 14:11:09 +0500

    added ability to set start date for a card

--- HappyTravel.VccServiceClient/Models/VccIssueRequest.cs
+++ HappyTravel.VccServiceClient/Models/VccIssueRequest.cs
@@ -5,16 +5,18 @@ namespace HappyTravel.VccServiceClient.Models
 {
     internal readonly struct VccIssueRequest
     {
-        public VccIssueRequest(string referenceCode, MoneyAmount moneyAmount, DateTime dueDate)
+        public VccIssueRequest(string referenceCode, MoneyAmount moneyAmount, DateTime startDate, DateTime dueDate)
         {
             ReferenceCode = referenceCode;
             MoneyAmount = moneyAmount;
+            StartDate = startDate;
             DueDate = dueDate;
         }
         
         
         public string ReferenceCode { get; }
         public MoneyAmount MoneyAmount { get; }
+        public DateTime StartDate { get; }
         public DateTime DueDate { get; }
     }
 }
